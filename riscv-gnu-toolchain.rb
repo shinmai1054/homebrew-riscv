0b1a262e4a0d50c668d745aca730dceafc692b98
shinmai1054 <46531958+shinmai1054@users.noreply.github.com>
2022-03-03 21:36:10 +0900

    Update riscv-gnu-toolchain.rb

--- riscv-gnu-toolchain.rb
+++ riscv-gnu-toolchain.rb
@@ -33,8 +33,8 @@ class RiscvGnuToolchain < Formula
       "--with-cmodel=medany",
     ]
     args << "--enable-multilib" unless build.with?("NOmultilib")
-    args << "--with-arch=rv32g" unless build.with?("RV32i")
-    args << "--with-abi=ilp32d" unless build.with?("RV32i")
+    args << "--with-arch=rv32i" unless build.with?("RV32i")
+    args << "--with-abi=ilp32" unless build.with?("RV32i")
 
     # Workaround for M1
     # See https://github.com/riscv/homebrew-riscv/issues/47
